70142e203c9a34514a37516a529c91324b8fb951
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-01-05 13:46:34 -0500

    Gatsby Tutorial pt. 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import Layout from '../components/layout'
 
-export default () => <div>Hello world!</div>
+export default () => 
+(
+  <Layout>
+  <div style={{ margin: `3rem auto`, maxWidth: 600}}>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial :)</h1>
+    <p>
+      What do I like to do? Lots of things, of course, but definitely including building websites. :)
+    </p>
+  </div>
+  </Layout>
+)
